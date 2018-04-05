49ddf66ac19a6233f32251de8101d89ef54eba34
ScaredTuna <jordanamcr@gmail.com>
2018-04-05 17:05:03 +0100

    added initial data file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Account (id, title, isbn, author, genre) VALUES ('1', 'Hello World', '45326754365476', 'Coders', 'Educational');
+INSERT INTO Account (id, title, isbn, author, genre) VALUES ('2', 'Madness', '352356436376', 'Cultists', 'Horror');
+INSERT INTO Account (id, title, isbn, author, genre) VALUES ('3', 'Once Upon a Time', '87546876432987', 'Some guy', 'Adventure');
